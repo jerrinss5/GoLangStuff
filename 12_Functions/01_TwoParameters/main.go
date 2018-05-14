a966803bf196818fd48a100d1090c67a6fbe6bb2
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-05-14 17:06:14 -0500

    Added comments

--- 12_Functions/01_TwoParameters/main.go
+++ 12_Functions/01_TwoParameters/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// arguments passed to the function
 	greet("Roronoa", "Zoro")
 }
 
+// functions declared with parameters
 func greet(fname, lname string) {
 	fmt.Println(fname, " ", lname)
 }
