48a9d953a00b8b3e1e6496d5a60a2d37b4aa2499
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-05-15 15:54:59 -0500

    Callback function which returns an integer

---
+++ 13_CallbackFunc/02_CallbackStartReturn/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// calling the visit function with slice value and funcition definition of int return type
+	visit([]int{1, 2, 3, 4, 5, 6}, func(n int) int {
+		return n
+	})
+}
+
+// declaring visit function which takes in slice and function value
+func visit(numbers []int, callback func(int) int) {
+	for _, n := range numbers {
+		fmt.Println(callback(n))
+	}
+}
