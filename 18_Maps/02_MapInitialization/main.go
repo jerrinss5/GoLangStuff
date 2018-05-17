4c1cb1313364819834f71d8f1c8ca9a047dcf663
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-05-17 11:22:08 -0500

    different forms for map initialization

---
+++ 18_Maps/02_MapInitialization/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// with make
+	var myMap = make(map[string]string)
+	fmt.Println(myMap == nil)
+
+	myMap["hello"] = "world"
+	myMap["bye"] = "bye"
+
+	fmt.Println(myMap)
+
+	// just declaration
+	var myMap1 map[string]string
+	fmt.Println(myMap1 == nil)
+
+	// with composite
+	var myMap2 = map[string]string{}
+	fmt.Println(myMap2 == nil)
+}
