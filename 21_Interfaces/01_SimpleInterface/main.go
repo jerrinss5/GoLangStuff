ec0a4ee8c6c768aa05cc245a886e1dc672b1288c
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-05-21 12:42:30 -0500

    Added circle shape as a new structure which implements area function returning float64

--- 21_Interfaces/01_SimpleInterface/main.go
+++ 21_Interfaces/01_SimpleInterface/main.go
@@ -1,14 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // Square : Square struct
 type Square struct {
 	side float64
 }
 
+// Circle : Circle struct
+type Circle struct {
+	radius float64
+}
+
 func (sq Square) area() float64 {
 	return sq.side * sq.side
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
 // Shape : interface which requires the function to implement area returning float64
@@ -26,4 +38,7 @@ func main() {
 	// passing the square variable - since the square implements area
 	// info accepts the square variable
 	info(s)
+
+	c := Circle{3}
+	info(c)
 }
