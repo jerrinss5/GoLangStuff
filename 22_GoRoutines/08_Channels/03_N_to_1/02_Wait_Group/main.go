81499d41fa397f7c91095689857f9a85736bb774
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-06-07 15:52:50 -0500

    Resolved the wait group race condition by moving the wg add outside the contention block

---
+++ 22_GoRoutines/08_Channels/03_N_to_1/02_Wait_Group/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	c := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		// race condition on wait group as it is within block
+		// wg.Add(1)
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		// race condition on wait group as it is within block
+		// wg.Add(1)
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	for n := range c {
+		fmt.Println(n)
+	}
+}
