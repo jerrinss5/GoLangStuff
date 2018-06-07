4a4eb3864badadae217e2a6701e021cba3757432
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-06-07 11:55:56 -0500

    Simulating race condition in GO

---
+++ 22_GoRoutines/05_RaceCondition/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var counter int
+
+func main() {
+	wg.Add(2)
+	go incrementor("Foo:")
+	go incrementor("Bar:")
+	wg.Wait()
+}
+
+func incrementor(s string) {
+	for i := 0; i < 20; i++ {
+		x := counter
+		x++
+		time.Sleep(time.Duration(rand.Intn(3)) * time.Millisecond)
+		counter = x
+		fmt.Println(s, i, "Counter: ", counter)
+	}
+	wg.Done()
+}
