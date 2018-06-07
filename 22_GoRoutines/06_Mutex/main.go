9a8b560216a6e4867c54663ebe258ee9c813067d
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-06-07 13:25:54 -0500

    Using mutex to solve the race condition problem

--- 22_GoRoutines/06_Mutex/main.go
+++ 22_GoRoutines/06_Mutex/main.go
@@ -9,9 +9,8 @@ import (
 
 var wg sync.WaitGroup
 var counter int
+var mutex sync.Mutex
 
-// when running the go program if we use
-// go run -race main.go - it will inform about the race condition
 func main() {
 	wg.Add(2)
 	go incrementor("Foo:")
@@ -21,11 +20,17 @@ func main() {
 
 func incrementor(s string) {
 	for i := 0; i < 20; i++ {
+		time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+		// getting the lock for this block of code
+		mutex.Lock()
+
 		x := counter
 		x++
-		time.Sleep(time.Duration(rand.Intn(3)) * time.Millisecond)
 		counter = x
 		fmt.Println(s, i, "Counter: ", counter)
+
+		mutex.Unlock()
+		// releasing the lock for other threads
 	}
 	wg.Done()
 }
