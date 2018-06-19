387799d5bd411ab0f182941ae27d614d315a5561
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-06-19 14:11:24 -0500

    Fan in Fan out model for factorial

---
+++ 22_GoRoutines/10_FanIn_FanOut/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	in := gen()
+
+	// FAN OUT
+	c0 := factorial(in)
+	c1 := factorial(in)
+	c2 := factorial(in)
+	c3 := factorial(in)
+	c4 := factorial(in)
+	c5 := factorial(in)
+	c6 := factorial(in)
+	c7 := factorial(in)
+	c8 := factorial(in)
+	c9 := factorial(in)
+
+	// FAN IN
+	for n := range merge(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) {
+		fmt.Println(n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			for j := 3; j < 13; j++ {
+				out <- j
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
+
+/*
+CHALLENGE #1:
+-- Change the code above to execute 1,000 factorial computations concurrently and in parallel.
+-- Use the "fan out / fan in" pattern
+
+CHALLENGE #2:
+WATCH MY SOLUTION BEFORE DOING THIS CHALLENGE #2
+-- While running the factorial computations, try to find how much of your resources are being used.
+-- Post the percentage of your resources being used to this discussion: https://goo.gl/BxKnOL
+*/
